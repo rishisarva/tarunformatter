4f8e474155f7025e070b6d931635a7b634c9b623
rishisarva <115390074+rishisarva@users.noreply.github.com>
2026-01-14 00:04:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # config.py
-
+BOT_TOKEN = "YOUR_BOT_TOKEN"
 FILE_ID_MAP_URL = "https://raw.githubusercontent.com/rishisarva/tarunformatter/main/telegram_file_ids.json"
 
 MAX_IMAGES_PER_REQUEST = 10
