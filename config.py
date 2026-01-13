9b5cac4625d215b85f200c0af5f32ee029dcb404
rishisarva <115390074+rishisarva@users.noreply.github.com>
2026-01-13 23:30:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # config.py
 
-FILE_ID_MAP_URL = "https://raw.githubusercontent.com/<your-username>/<repo>/main/telegram_file_ids.json"
+FILE_ID_MAP_URL = "https://raw.githubusercontent.com/rishisarva/tarunformatter/main/telegram_file_ids.json"
 
 MAX_IMAGES_PER_REQUEST = 10
