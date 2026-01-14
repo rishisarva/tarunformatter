0e602647586bdaa90f3a41e98709f30a44606647
rishisarva <115390074+rishisarva@users.noreply.github.com>
2026-01-14 14:17:28 +0530

    Update image_sender.py

--- image_sender.py
+++ image_sender.py
@@ -1,21 +1,21 @@
+# image_sender.py
 import random
-from config import MAX_IMAGES_PER_REQUEST
+import asyncio
+
+IMAGE_DELAY = 0.6  # prevents Telegram flood block
+MAX_IMAGES = 10
+
 
 async def send_images(bot, chat_id, images):
-    """
-    images = list of {name, file_id}
-    """
     if not images:
         await bot.send_message(chat_id, "❌ No jerseys found")
         return
 
-    selected = random.sample(
-        images,
-        min(MAX_IMAGES_PER_REQUEST, len(images))
-    )
+    selected = random.sample(images, min(MAX_IMAGES, len(images)))
 
     for img in selected:
         await bot.send_photo(
             chat_id=chat_id,
             photo=img["file_id"]
         )
+        await asyncio.sleep(IMAGE_DELAY)
