fb02d3d3fe2bc4c599a608a37effbb606a37ba4c
rishisarva <115390074+rishisarva@users.noreply.github.com>
2026-01-14 21:39:52 +0530

    Update state.py

--- state.py
+++ state.py
@@ -1,7 +1,12 @@
-# state.py
+_state = {}
 
-state = {}
+def get(uid, key=None):
+    if key:
+        return _state.get(uid, {}).get(key)
+    return _state.get(uid, {})
 
-def set(uid, k, v): state.setdefault(uid,{})[k]=v
-def get(uid, k): return state.get(uid,{}).get(k)
-def clear(uid): state[uid]={}
+def set(uid, key, value):
+    _state.setdefault(uid, {})[key] = value
+
+def clear(uid):
+    _state.pop(uid, None)
