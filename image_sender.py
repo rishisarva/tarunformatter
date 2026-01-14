dbeb17ba8018474ea1412102ea87d9deb7d4d887
rishisarva <115390074+rishisarva@users.noreply.github.com>
2026-01-14 14:41:30 +0530

    Update image_sender.py

--- image_sender.py
+++ image_sender.py
@@ -4,22 +4,19 @@ from telegram import InputMediaPhoto
 
 MAX_IMAGES = 10
 
-
 async def send_images(bot, chat_id, images):
     if not images:
         await bot.send_message(chat_id, "❌ No jerseys found")
         return
 
-    # pick random images (max 10)
     selected = random.sample(images, min(MAX_IMAGES, len(images)))
 
-    media_group = [
+    media = [
         InputMediaPhoto(media=img["file_id"])
         for img in selected
     ]
 
-    # 🔥 SEND AS ONE GROUP
     await bot.send_media_group(
         chat_id=chat_id,
-        media=media_group
+        media=media
     )
