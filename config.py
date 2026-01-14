ce49975eb595f0f93f41371f3778db3b67f0e1d7
rishisarva <115390074+rishisarva@users.noreply.github.com>
2026-01-14 14:41:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,12 @@
 # config.py
-import os
 import requests
+import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-if not BOT_TOKEN:
-    raise RuntimeError("BOT_TOKEN not set in environment")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")  # set in Render ENV
 
 FILE_ID_MAP_URL = "https://raw.githubusercontent.com/rishisarva/tarunformatter/main/telegram_file_ids.json"
 
-resp = requests.get(FILE_ID_MAP_URL, timeout=15)
+resp = requests.get(FILE_ID_MAP_URL, timeout=20)
 resp.raise_for_status()
 TELEGRAM_FILE_MAP = resp.json()
 
