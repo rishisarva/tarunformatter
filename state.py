83ce02e7e2533b0ef6fdacde30bba55d10140da7
rishisarva <115390074+rishisarva@users.noreply.github.com>
2026-01-25 01:10:45 +0530

    Update state.py

--- state.py
+++ state.py
@@ -10,3 +10,15 @@ def set(uid, key, value):
 
 def clear(uid):
     _state.pop(uid, None)
+
+MAX_RECENT = 9
+
+def push_recent(uid, key, value):
+    user = _state.setdefault(uid, {})
+    recents = user.setdefault(key, [])
+
+    if value in recents:
+        recents.remove(value)
+
+    recents.insert(0, value)
+    del recents[MAX_RECENT:]
